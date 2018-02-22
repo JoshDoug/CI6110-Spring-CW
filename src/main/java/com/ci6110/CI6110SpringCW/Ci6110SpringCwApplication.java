35bd7ea4eee32e7cfe9f02eecfe609420e70cef1
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-02-22 15:12:02 +0000

    Add base project

---
+++ src/main/java/com/ci6110/CI6110SpringCW/Ci6110SpringCwApplication.java
@@ -0,0 +1,12 @@
+package com.ci6110.CI6110SpringCW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ci6110SpringCwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ci6110SpringCwApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ci6110/CI6110SpringCW/Ci6110SpringCwApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ci6110.CI6110SpringCW;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ci6110SpringCwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
