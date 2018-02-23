b42759624bf2d4c2aec0c30f5f5a1d752af326b4
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-02-23 11:32:37 +0000

    Add employee MVC setup

--- src/main/java/com/ci6110/main/InputController.java
+++ src/main/java/com/ci6110/main/InputController.java
@@ -1,14 +1,28 @@
 package com.ci6110.main;
 
+import com.ci6110.beans.Employee;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class InputController {
 
-    @RequestMapping("submit") // Note: can use GetMapping to scope to just Get HTTP Requests
-    public String employeeForm() {
-        return "greeting";
+//    @RequestMapping("submit") // Note: can use GetMapping to scope to just Get HTTP Requests
+//    public String employeeForm() {
+//        return "greeting";
+//    }
+
+    @GetMapping("/employee")
+    public String employeeForm(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "employee";
     }
 
+    @PostMapping("/employee")
+    public String employeeSubmit(@ModelAttribute Employee employee) {
+        return "employeeSubmit";
+    }
 }
