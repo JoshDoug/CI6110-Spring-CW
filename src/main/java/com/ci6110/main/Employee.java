d954a27feeedc2527f92b1b72ef2928661dc981b
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-03-04 20:29:09 +0000

    Move and Update Employee bean
    Map Employee Entity to DB Table

---
+++ src/main/java/com/ci6110/main/Employee.java
@@ -0,0 +1,63 @@
+package com.ci6110.main;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="EMPLOYEE")
+public class Employee {
+
+    @Id
+    @Column(name="EMPLOYEE_ID")
+    @GeneratedValue
+    private long id;
+    @Column(name="FIRST_NAME")
+    private String firstName;
+    @Column(name="LAST_NAME")
+    private String lastName;
+    @Column(name="SALARY")
+    private float salary; // Might make more sense to use BigDecimal here
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(long id, String firstName, String lastName, float salary) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String name) {
+        this.lastName = name;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
